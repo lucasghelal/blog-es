67dfdec63bd022d51e059cdbe1c17febe2456b44
lucas helal <lucasghelal@gmail.com>
2016-07-09 05:31:31 -0300

    Concluido busca por similaridades (tags)

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -55,9 +56,20 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post,
-                                                     'comments': comments,
-                                                     'comment_form': comment_form})
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+                         tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+                         same_tags=Count('tags')).order_by('-same_tags',
+                                                           '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html',
+                           {'post': post,
+                            'comments': comments,
+                            'comment_form': comment_form,
+                            'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
