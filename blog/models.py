fcd103b2645e34694ef84aa95092a2a48601b140
lucas helal <lucasghelal@gmail.com>
2016-07-09 04:47:38 -0300

    Criado Comentarios

--- blog/models.py
+++ blog/models.py
@@ -39,3 +39,19 @@ class Post(models.Model):
 
         def __str__(self):
             return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
