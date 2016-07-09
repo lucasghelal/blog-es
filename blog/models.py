b274cf8077b10ff496e298826c74b5e683369b3f
lucas helal <lucasghelal@gmail.com>
2016-07-09 02:54:23 -0300

    Criado o blog

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class PublishedManager(models.Manager):
@@ -25,6 +26,13 @@ class Post(models.Model):
                               choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 
     class Meta:
         ordering = ('-publish',)
