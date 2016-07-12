bfd38ec15a903d267d4e7c76136a7cf469e16438
lucas helal <lucasghelal@gmail.com>
2016-07-11 23:24:44 -0300

    Fixed warming das urls

--- account/urls.py
+++ account/urls.py
@@ -1,32 +1,33 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     # post views
     # url(r'^login/$', views.user_login, name='login'),
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login',
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout-then-login/$', auth_views.logout_then_login,
         name='logout_then_login'),
     url(r'^$', views.dashboard, name='dashboard'),
     # change password urls
-    url(r'^password-change/$', 'django.contrib.auth.views.password_change',
+    url(r'^password-change/$', auth_views.password_change,
         name='password_change'),
     url(r'^password-change/done/$',
-        'django.contrib.auth.views.password_change_done',
+        auth_views.password_change_done,
         name='password_change_done'),
     # restore password urls
     url(r'^password-reset/$',
-        'django.contrib.auth.views.password_reset',
+        auth_views.password_reset,
         name='password_reset'),
     url(r'^password-reset/done/$',
-        'django.contrib.auth.views.password_reset_done',
+        auth_views.password_reset_done,
         name='password_reset_done'),
     url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
-        'django.contrib.auth.views.password_reset_confirm',
+        auth_views.password_reset_confirm,
         name='password_reset_confirm'),
     url(r'^password-reset/complete/$',
-        'django.contrib.auth.views.password_reset_complete',
+        auth_views.password_reset_complete,
         name='password_reset_complete'),
 
     url(r'^register/$', views.register, name='register'),
