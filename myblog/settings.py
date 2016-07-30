5edb29aac395d90011929d920b5e902c1526e9a2
lucas helal <lucasghelal@gmail.com>
2016-07-30 03:24:41 -0300

    Material Design no admin

--- myblog/settings.py
+++ myblog/settings.py
@@ -40,6 +40,8 @@ SITE_ID = 2
 
 INSTALLED_APPS = [
     'account',
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
